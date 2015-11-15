52acefa37007559641fca1aed1dfb184442a98f9
Bartosz Bednarczyk <bartosz.jan.bednarczyk@gmail.com>
2015-11-15 22:51:42 +0100

    pracownia-kolejne-zmiany

---
+++ 2015-2016/Analiza numeryczna M/Pracownia/Pracownia 1/prog/funkcje.jl
@@ -0,0 +1,43 @@
+function solveQuadratic(a, b, c)
+
+  delta = b*b - 4 * a * c
+
+  if delta < 0
+    x1 = Complex(-b/(2*a), -sqrt(-delta)/(2*a))
+    x2 = Complex(-b/(2*a), +sqrt(-delta)/(2*a))
+    return [x1, x2]
+
+  else
+    x1 = Complex((-b - sqrt(delta))/(2*a))
+    x2 = Complex((-b + sqrt(delta))/(2*a))
+    return [x1, x2]
+  end
+
+end
+
+#funkcja konwertujaca String na Array{BigFloat}
+function strb(a::ASCIIString)
+  prec = get_bigfloat_precision()
+  set_bigfloat_precision(1024)
+  a = replace(a, "[", "") 
+  a = replace(a, "]", "") 
+  a = replace(a, ",", " ") 
+  a = split(a)
+
+  a = map(x -> parse(BigFloat, x), a)
+
+  set_bigfloat_precision(prec)
+  return a
+end
+
+#funkcja dzileaca wielomian przez czynnik (x-x0)
+function horner(a, x0)
+  n = length(a) - 1
+  b = Array(Complex{BigFloat}, n)
+  b[n] = a[n+1]
+  for _k in 0:(n-2)
+    i = n - _k
+    b[i-1] = a[i] + b[i]*x0
+  end
+  return b
+end
---
+++ 2015-2016/Analiza numeryczna M/Pracownia/Pracownia 1/prog/program.jl
@@ -0,0 +1,9 @@
+include("funkcje.jl")
+include("lagguerre.jl")
+include("bairstow.jl")
+include("muller.jl")
+include("testy.jl")
+
+EPS = BigFloat(1e-50)
+set_bigfloat_precision(128)
+
